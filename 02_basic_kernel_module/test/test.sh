4ea5e9d3ac61849074e0bcae707c87c8f3293a38
Francisco-Raspberry <fco.torres12@gmail.com>
2025-09-01 23:47:14 +0100

    Fix a typo in 02_basic_kernel_module/test/test.sh

--- 02_basic_kernel_module/test/test.sh
+++ 02_basic_kernel_module/test/test.sh
@@ -13,7 +13,7 @@ dmesg | tail -n 20 > load.log
 grep "Hello" load.log || echo "Expected output has not been found in dmesg"
 
 echo "[+] Removing module ..."
-sudo rmmod ../src/hello.ko || { echo "Error while inserting"; exit 1; }
+sudo rmmod ../src/hello.ko || { echo "Error while removing"; exit 1; }
 
 echo "[+] Verifying using dmesg ..."
 dmesg | tail -n 20 > load.log
